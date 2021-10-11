28321f770620d9b0b616d674ab0db3c4f9a0df7b
Gay <konako.osu@gmail.com>
2021-10-11 16:48:02 +0500

    Bugfix and improvements

--- database.py
+++ database.py
@@ -1,8 +1,8 @@
 import aiosqlite
-from secret_chat.config import main_dir
 from typing import Optional
 from dataclasses import dataclass
 from datetime import datetime
+from pathlib import *
 
 # DONE TODO: get set say nums
 # DONE TODO: get update sticker date, get update sticker prob, get update sticker count
@@ -42,7 +42,7 @@ class Frame:
 
 
 class Db:
-    def __init__(self, path=main_dir + 'ls.db'):
+    def __init__(self, path=Path.cwd()/'ls.db'):
         self._conn = aiosqlite.connect(path)
 
     async def connect(self):
@@ -53,8 +53,8 @@ class Db:
                                  'frame INTEGER NOT NULL,'
                                  'count INTEGER NOT NULL, '
                                  'datetime FLOAT NOT NULL)')
-        await self._conn.execute('CREATE TABLE IF NOT EXISTS stickers(sticker TEXT PRIMARY KEY NOT NULL ,'
-                                 'date FLOAT NOT NULL,'
+        await self._conn.execute('CREATE TABLE IF NOT EXISTS stickers(sticker TEXT PRIMARY KEY NOT NULL,'
+                                 'datetime FLOAT NOT NULL,'
                                  'probability INTEGER NOT NULL,'
                                  'count INTEGER NOT NULL)')
         await self._conn.execute('CREATE TABLE IF NOT EXISTS stat(stat INTEGER PRIMARY KEY NOT NULL,'
@@ -66,15 +66,27 @@ class Db:
         await self._conn.commit()
         await self._conn.close()
 
+    async def __aenter__(self):
+        await self.connect()
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        await self.close()
+        return None
+
     async def add_frame(self, frame: int, recent_datetime: float):
         frame_data = await self.get_frame_stat(frame)
-        frame_count = frame_data.count
-        if frame_count is None:
-            await self._conn.execute('INSERT INTO frames(frame, count, recentDatetime) VALUES (?, ?, ?)',
+        if frame_data is None:
+            await self._conn.execute('INSERT INTO frames(frame, count, datetime) VALUES (?, ?, ?)',
                                      (frame, 1, recent_datetime))
         else:
-            await self._conn.execute('UPDATE frames SET frame=?, recentDatetime=? WHERE count=? ',
-                                     (frame, recent_datetime, frame_count + 1))
+            await self._conn.execute('UPDATE frames SET frame=?, datetime=? WHERE count=? ',
+                                     (frame, recent_datetime, frame_data.count + 1))
+        await self._conn.commit()
+
+    async def add_sticker(self, sticker: StickerInfo):
+        await self._conn.execute('INSERT INTO stickers(sticker, datetime, probability, count) VALUES(?, ?, ?, ?)',
+                                 (sticker.name, sticker.date.timestamp(), sticker.probability, 0))
         await self._conn.commit()
 
     async def add_paste(self, text: str):
@@ -92,20 +104,25 @@ class Db:
             await self._conn.execute('INSERT INTO calls(sign, num) VALUES (?, ?)',
                                      (sign, num))
         else:
-            num_to_save = num if num > saved_num else saved_num
-            await self._conn.execute('UPDATE calls SET sign=? WHERE num=?',
-                                     (sign, num_to_save))
+            if sign == 'positive':
+                num_to_save = num if num > saved_num else saved_num
+            else:
+                num_to_save = num if num < saved_num else saved_num
+            await self._conn.execute('UPDATE calls SET num=? WHERE sign=?',
+                                     (str(num_to_save), sign))
         await self._conn.commit()
 
     async def update_sticker(self, sticker: StickerInfo):
         previous_sticker = await self.get_sticker_info(sticker.name)
 
         if previous_sticker is None:
-            await self._conn.execute('INSERT INTO stickers(sticker, date, probability, count) VALUES (?, ?, ?, ?)',
-                                     (sticker.name, sticker.date.timestamp, sticker.probability, 1))
+            args = (sticker.name, sticker.date.timestamp(), sticker.probability, sticker.count)
+            print(args)
+            await self._conn.execute('INSERT INTO stickers(sticker, datetime, probability, count) VALUES (?, ?, ?, ?)',
+                                     args)
         else:
-            await self._conn.execute('UPDATE stickers SET date=?, probability=?, count=? WHERE sticker=?',
-                                     (sticker.date.timestamp, sticker.probability, previous_sticker.count + 1, sticker.name))
+            await self._conn.execute('UPDATE stickers SET datetime=?, probability=?, count=? WHERE sticker=?',
+                                     (sticker.date.timestamp(), sticker.probability, previous_sticker.count + 1, sticker.name))
         await self._conn.commit()
 
     async def update_stat(self, stat_type: int):
@@ -157,26 +174,26 @@ class Db:
         return None
 
     async def get_paste_count(self) -> Optional[int]:
-        cur = await self._conn.execute('COUNT(*)')
+        cur = await self._conn.execute('SELECT COUNT(*) FROM pastes')
         row = await cur.fetchone()
         if row is not None:
             return row[0]
         return None
 
-    async def get_num(self, sign: str) -> Optional[int]:
+    async def get_num(self, sign: str) -> Optional[float]:
         cur = await self._conn.execute('SELECT num FROM calls WHERE sign=?',
                                        (sign, ))
         row = await cur.fetchone()
         if row is not None:
-            return row[0]
+            return float(row[0])
         return None
 
     async def get_sticker_info(self, sticker: str) -> Optional[StickerInfo]:
-        cur = await self._conn.execute('SELECT date, probability, count FROM stickers WHERE sticker=?',
+        cur = await self._conn.execute('SELECT datetime, probability, count FROM stickers WHERE sticker=?',
                                        (sticker, ))
         row = await cur.fetchone()
         if row is not None:
-            return StickerInfo(sticker, datetime.fromtimestamp(row[0]), row[1], row[2])
+            return StickerInfo(sticker, datetime.fromtimestamp(row[0]), int(row[1]), int(row[2]))
         return None
 
     async def get_statistics(self, stat: int) -> Optional[int]:
