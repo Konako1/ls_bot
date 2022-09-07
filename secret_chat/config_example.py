60c490ea66382790f18c7e1f0d2d6ffba6a092b3
Konako <konako.osu@gmail.com>
2022-09-07 14:33:38 +0500

    add config example

---
+++ secret_chat/config_example.py
@@ -0,0 +1,13 @@
+TG_TOKEN = '1234567890:zxcvbnmasdfghjklqwertyuiop'  # tg bot token at https://t.me/BotFather 
+VK_TOKEN = 'key here'  # сервисный ключ доступа at https://vk.com/editapp?id=1234567890&section=options
+OWM_API = "your api"  # https://home.openweathermap.org/api_keys
+chat_id = 1234567890  # your chat id
+ls_group_id = -1234567890  # your and your friends group id
+test_group_id = -1234567890  # your test group id
+path = 'C:/Users/user/.../project'  # path to project
+frames_dir = 'C:/Users/user/.../random-frame-as-tg-pfp/all_frames/'  # frames directory
+spring_05_preview_direction = 'C:/Users/user/.../tm_spring_05.png'  # kek
+traveling_days = '【東方Vocal／Eurobeat】 トラベリング・デイズ 「Crazy Beats」'  #kek v2
+users = {
+            "name": 1234567890
+} # "name": id  for better naming (i think outdated)
